70e470c4ce2687a64385ee71f99aa4ec302bc77f
Nick <boodoo@temple.edu>
2024-03-13 16:32:30 -0400

    added new files

--- soul.py
+++ soul.py
@@ -4,6 +4,17 @@ from sys import exit
 pygame.init()
 
 screen = pygame.display.set_mode((800,400))
+pygame.display.set_caption('Project S')
+
+clock = pygame.time.Clock()
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
+
+sky_surface = pygame.image.load('graphics/Sky.png').convert()
+ground_surface = pygame.image.load('graphics/ground.png').convert()
+text_surface = test_font.render('My game', False, 'Black')
+
+snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_x_pos = 600
 
 while True:
     for event in pygame.event.get():
@@ -11,6 +22,15 @@ while True:
             pygame.quit()
             exit()
 
-    # draw all of our elements
-    # update everything
+    screen.blit(sky_surface, (0,0))
+    screen.blit(ground_surface, (0,300))
+    screen.blit(text_surface,(300, 50))
+
+    snail_x_pos -= 4
+    if snail_x_pos < -100:
+        snail_x_pos = 800
+
+    screen.blit(snail_surface,(snail_x_pos, 250))
+
     pygame.display.update()
+    clock.tick(60)
